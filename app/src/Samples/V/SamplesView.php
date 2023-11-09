811a28cdd1ffc96cb4df982b376b606a666c45f5
NovosDmytro <novosdmytro@gmail.com>
2023-11-09 20:17:37 +0100

    mvca-32 undone

--- app/src/Samples/V/SamplesView.php
+++ app/src/Samples/V/SamplesView.php
@@ -9,4 +9,9 @@ IMPORTANT! Basic php global variables $_GET, $_POST, $_COOKIES, $_SERVER is unse
 
 
 <br>
+<br><br>
 <br>
+<h1>Basic MVC example</h1>
+This is ultra light basic sample how to work with this mvc<br>
+<a href="/Samples-Basics-main">Show sample</a><br>
+
