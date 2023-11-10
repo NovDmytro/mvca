acb2131699db0703b07c97ab4dd2761b54bdc0c5
PimiTree <voronenkotg@gmail.com>
2023-11-10 03:23:39 +0200

    mvca-30: add cokies and basics

--- app/src/Samples/V/SamplesView.php
+++ app/src/Samples/V/SamplesView.php
@@ -1,18 +1,26 @@
 <section class="sample">
-<h1 class="sample__header">MVCA Requests</h1>
-<div class="sample__container">
-    <div class="sample-request__left">
-        <p class="sample__text sample__info">MVCA Requests handle <span class="sample__text-important">$_GET</span>, <span class="sample__text-important">$_POST</span>, <span class="sample__text-important">$_COOKIES</span>, <span class="sample__text-important">$_SERVER</span> and <span class="sample__text-important">custom JSON</span>  requests.</p>
-        <div class="sample__links-container">
-            <a class="sample__link" href="/Samples-Requests-getSamples">GET Request samples</a>
-            <a class="sample__link" href="/Samples-Requests-postSamples">POST Request samples</a>
-            <a class="sample__link" href="/Samples-Requests-jsonSamples">JSON Request samples</a>
-            <a class="sample__link" href="/Samples-Requests-serverSamples">SERVER Request samples</a>
+    <h1 class="sample__header">MVCA Requests</h1>
+    <div class="sample__container">
+        <div class="sample-request__left">
+            <p class="sample__text sample__info">MVCA Requests handle <span class="sample__text-important">$_GET</span>, <span class="sample__text-important">$_POST</span>, <span class="sample__text-important">$_COOKIES</span>, <span class="sample__text-important">$_SERVER</span> and <span class="sample__text-important">custom JSON</span>  requests.</p>
+            <div class="sample__links-container">
+                <a class="sample__link" href="/Samples-Requests-getSamples">GET Request samples</a>
+                <a class="sample__link" href="/Samples-Requests-postSamples">POST Request samples</a>
+                <a class="sample__link" href="/Samples-Requests-jsonSamples">JSON Request samples</a>
+                <a class="sample__link" href="/Samples-Requests-serverSamples">SERVER Request samples</a>
+            </div>
         </div>
+        <div class="sample-request__right">
+            <p class="sample__text"><span class="sample__text warning">IMPORTANT!</span> MVCA Requests provide unsetting the basic php global variables, such as <span class="sample__text important">$_GET</span>, <span class="sample__text important">$_POST</span>, <span class="sample__text important">$_COOKIES</span>, <span class="sample__text important">$_SERVER</span> to serve additional safety.</p>
+        </div>    
     </div>
-    <div class="sample-request__right">
-        <p class="sample__text"><span class="sample__text warning">IMPORTANT!</span> MVCA Requests provide unsetting the basic php global variables, such as <span class="sample__text important">$_GET</span>, <span class="sample__text important">$_POST</span>, <span class="sample__text important">$_COOKIES</span>, <span class="sample__text important">$_SERVER</span> to serve additional safety.</p>
-    </div>
-    
-</div>
+    <h1>Cookies</h1><br>
+    Framework has service to work with cookies.<br>
+    <a href="/Samples-Cookies-main">Cookies samples</a><br>
+
+
+    <br><br>
+    <h1>Basic MVC example</h1>
+    This is ultra light basic sample how to work with this mvc<br>
+    <a href="/Samples-Basics-main">Show sample</a><br>
 </section>
