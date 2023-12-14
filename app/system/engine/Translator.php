1c85fcc3be8c152bfaf092413ded5556d70f24ec
NovosDmytro <novosdmytro@gmail.com>
2023-12-14 19:17:46 +0100

    mvca-41 multyproject patch (need to update readme and samples)

--- app/system/engine/Translator.php
+++ app/system/engine/Translator.php
@@ -5,17 +5,19 @@ namespace Engine;
 class Translator
 {
     protected string $lang;
+    private string $path;
     protected array $translations;
 
-    public function __construct($lang)
+    public function __construct($lang,$path)
     {
         $this->lang = $lang;
+        $this->path = $path;
         $this->loadTranslations();
     }
 
     private function loadTranslations(): void
     {
-        $file = 'system/translations/' . $this->lang . '.php';
+        $file = $this->path. $this->lang . '.php';
         if (file_exists($file)) {
             $this->translations = require $file;
         }
@@ -24,7 +26,7 @@ class Translator
     public function translate($key)
     {
         if (isset($this->translations[$key])) {
-            return htmlspecialchars($this->translations[$key], ENT_QUOTES, 'UTF-8');
+            return htmlspecialchars($this->translations[$key], ENT_QUOTES);
         } else {
             return $key;
         }
